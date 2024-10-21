03a1491d59655436c0b1c11ed5e26585781a7296
Sarthak Mundhra <63492158+stkmnd@users.noreply.github.com>
2024-10-21 00:04:48 -0400

    Update Exercise_2.py

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,22 @@
 # Python program for implementation of Quicksort Sort 
-  
-# give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
-  
 
-# Function to do Quick sort 
-def quickSort(arr,low,high): 
-    
-    #write your code here
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quickSort(arr, low, high):
+    if low < high:
+        p = partition(arr, low, high)
+        quickSort(arr, low, p - 1)
+        quickSort(arr, p + 1, high)
   
 # Driver code to test above 
 arr = [10, 7, 8, 9, 1, 5] 
@@ -18,6 +24,4 @@ n = len(arr)
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+    print(arr[i]) 
