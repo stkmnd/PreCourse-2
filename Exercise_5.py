2c1e70487cbca4650d0ecc173b5fb76c93be30c9
Sarthak Mundhra <63492158+stkmnd@users.noreply.github.com>
2024-10-21 00:37:14 -0400

    Update Exercise_5.py

--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,27 @@
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1 
 
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
 
-def quickSortIterative(arr, l, h):
-  #write your code here
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
 
+def quickSortIterative(arr):
+    stack = []
+    stack.append((0, len(arr) - 1))
+
+    while stack:
+        low, high = stack.pop()
+
+        if low < high:
+            pivot = partition(arr, low, high)
+
+            stack.append((low, pivot - 1))
+            stack.append((pivot + 1, high))
